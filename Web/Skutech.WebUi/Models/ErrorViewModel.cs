5244a3ce0c86edabf0f3c5cea6a25616bdf2666b
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2025-03-31 14:23:34 -0400

    added web project

---
+++ Web/Skutech.WebUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Skutech.WebUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
