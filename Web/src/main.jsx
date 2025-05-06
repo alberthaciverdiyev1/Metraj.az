86f389e1b22a0c2f600a1708666cbdab210ff139
Albert <alberthaciverdiyev55@gmail.com>
2025-05-06 03:54:54 -0400

    one project for admin/api/web

--- Web/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./assets/style/index.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-  </>
-);
