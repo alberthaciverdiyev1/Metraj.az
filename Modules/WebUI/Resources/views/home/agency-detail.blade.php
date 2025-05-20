8cb384d3399a993f2c3bee5fe21b19c6ec210e8d
arzummammadova <arzuam-af106@code.edu.az>
2025-05-20 12:06:11 +0400

    addding new section

--- Modules/WebUI/Resources/views/home/agency-detail.blade.php
+++ Modules/WebUI/Resources/views/home/agency-detail.blade.php
@@ -121,8 +121,45 @@
                         </div>
 
                     </section>
-                    <div class="featured-listing">
-                        <!-- burda ele bilki butun data ilk 5 denesini cixartmaliyam -->
+                    <div class="feature-listings">
+                        <h3>Featured Listings</h3>
+                        <div class="featured-listings-cards">
+
+                            <!-- Card 1 -->
+                            <div class="featured-listing-card">
+                                <figure>
+                                    <img class="image-agency" src="https://themesflat.co/html/proty/images/section/box-listing-1.jpg" alt="House Image">
+                                </figure>
+                                <div class="card-info">
+                                    <h4>Casa Lomas de Machalí Machas</h4>
+                                    <div class="details">
+                                        <span>3 <span class="grey">Bed</span></span>
+                                        <span>3 <span class="grey">Bath</span></span>
+                                        <span>4,043 <span class="grey">Sqft</span></span>
+                                    </div>
+                                    <div class="price">$7250,00</div>
+                                </div>
+                            </div>
+                            <div class="featured-listing-card">
+                                <figure>
+                                    <img class="image-agency" src="https://themesflat.co/html/proty/images/section/box-listing-1.jpg" alt="House Image">
+                                </figure>
+                                <div class="card-info">
+                                    <h4>Casa Lomas de Machalí Machas</h4>
+                                    <div class="details">
+                                        <span>3 <span class="grey">Bed</span></span>
+                                        <span>3 <span class="grey">Bath</span></span>
+                                        <span>4,043 <span class="grey">Sqft</span></span>
+                                    </div>
+                                    <div class="price">$7250,00</div>
+                                </div>
+                            </div>
+
+
+
+
+
+                        </div>
                     </div>
                     <x-connect-agent />
                 </div>
