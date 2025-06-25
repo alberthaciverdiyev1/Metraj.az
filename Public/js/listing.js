6b701791b83ca626b6ce0f48f80c5362b0bb9d7a
arzummammadova <arzuam-af106@code.edu.az>
2025-06-25 20:11:51 +0400

    added new premiumsection

--- Public/js/listing.js
+++ Public/js/listing.js
@@ -1,6 +1,6 @@
 import {getPropertiesList} from "./components/property.js";
 import {propertyCard} from "./cards/property.js";
-
+import {premiumCard} from './cards/premiumCards.js'
 const gotop = document.getElementById('scrollToTop');
 const progress = document.querySelector('.progress-circle .progress');
 const radius = 18;
@@ -186,14 +186,26 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 
     async function properties() {
-        let properties = await getPropertiesList()
-        let h = '';
+        let properties = await getPropertiesList();
+    
+        let allCards = '';
+        let premiumCards = '';
+    
         properties.forEach(property => {
-            h += propertyCard(property);
-        })
-
-        container.innerHTML = h;
+            if (property.is_premium) {
+                premiumCards += premiumCard(property); 
+            }
+            allCards += propertyCard(property); 
+        });
+    
+        document.getElementById('premiumCard').innerHTML = premiumCards;
+        document.getElementById('propertyContainer').innerHTML = allCards;
+    
+        showPage(1);
     }
+    
+    
+
     document.querySelector('[data-role="property-type"]').addEventListener('change', async (event) => {
         const selectedValue = event.target.value;
         console.log('Selected value:', selectedValue);
