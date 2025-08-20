4516943375113c611f44e44986442f9781f57ee5
nazrin16h <nazrinhajizade16@gmail.com>
2025-08-20 17:56:50 +0400

    add default badges for rent/sale, repair, credit, document

--- Public/js/cards/property.js
+++ Public/js/cards/property.js
@@ -1,4 +1,4 @@
-import {formatPrice} from '../helpers/price.js';
+import { formatPrice } from '../helpers/price.js';
 
 
 function getCompareStatus(propertyId) {
@@ -18,73 +18,83 @@ function getCookie(name) {
 }
 
 export function propertyCard(property, showRemoveButton = false) {
+    // console.log(property);
+
+    const badges = `
+<div class="absolute top-3 left-3 flex flex-wrap  gap-2">
+    ${property.add_type === 'rent' ? `<span class="bg-[color:var(--primary)] text-white text-xs font-semibold px-2 text-center py-1 rounded-full">Kirayə</span>` :
+            property.add_type === 'sale' ? `<span class="bg-[#80807F] text-white text-xs font-semibold px-2 py-1 rounded-full">Satışda</span>` : ''}
+
+    ${property.property_condition === 'REPAIRED' ? `<span class="bg-green-600 text-white text-xs font-semibold px-2 text-center  py-1 rounded-full">Təmirli</span>` :
+            `<span class="bg-gray-400 text-white text-xs font-semibold px-2 py-1 text-center rounded-full">Təmirsiz</span>`}
+
+    ${property.in_credit ? `<span class="bg-blue-600 text-white text-xs font-semibold px-2 py-1 text-center  rounded-full">İpoteka var</span>` :
+            `<span class="bg-gray-400 text-white text-xs font-semibold px-2 py-1 text-center rounded-full">İpotekasız</span>`}
+
+    ${property.document === 'kupça var' ? `<span class="bg-green-500 text-white text-xs font-semibold px-2 py-1 text-center  rounded-full">Çıxarışlı</span>` :
+            `<span class="bg-red-500 text-white text-xs font-semibold px-2 py-1 text-center rounded-full">Çıxarışsız</span>`}
+</div>
+`;
+
     const rawPrice = property.price && property.price[0]?.price;
     let priceValue = 0;
-
-    if (rawPrice) {
-        priceValue = parseFloat(rawPrice.toString().replace(/,/g, ''));
-    }
-
+    if (rawPrice) priceValue = parseFloat(rawPrice.toString().replace(/,/g, ''));
     const price = formatPrice(priceValue);
 
     const premiumBadge = property.is_premium ? `<span class="absolute top-3 right-4 text-black font-semibold text-md bg-white px-2 py-1 rounded-full"><i class="fa-solid fa-crown"></i></span>` : '';
 
-    const addTypeBadge = property.add_type === 'rent' ? `<span class="bg-[color:var(--primary)] text-white text-sm font-semibold px-2 py-1 rounded-full">Kirayə</span>`
-                                                             : property.add_type === 'sale' ? `<span class="bg-[#80807F] text-white font-semibold text-sm px-2 py-1 rounded-full">Satışda</span>` : '';
-
     const heartIconClass = getFavoriteStatus(property.id) ? 'fa-solid' : 'fa-regular';
-
     const propertyData = encodeURIComponent(JSON.stringify(property));
-
-    const favoriteOrRemoveButton = showRemoveButton ? `<span onclick="event.stopPropagation(); removeFavorite(${property.id});" class="absolute bottom-3 right-4 text-red-500 font-semibold text-md bg-white px-2 py-1 rounded-full cursor-pointer"><i class="fas fa-times"></i></span>`
-                                                           : `<span onclick="event.stopPropagation(); toggleFavorite(this, decodeURIComponent('${propertyData}'));" class="absolute bottom-3 right-4 text-white font-semibold text-md bg-white px-2 py-1 rounded-full cursor-pointer"><i class="${heartIconClass} fa-heart text-red-500"></i></span> `;
+    const favoriteOrRemoveButton = showRemoveButton ? `<span onclick="event.stopPropagation(); removeFavorite(${property.id});" class="absolute bottom-3 right-4 text-red-500 font-semibold text-md bg-white px-2 py-1 rounded-full cursor-pointer"><i class="fas fa-times"></i></span>` :
+        `<span onclick="event.stopPropagation(); toggleFavorite(this, decodeURIComponent('${propertyData}'));" class="absolute bottom-3 right-4 text-white font-semibold text-md bg-white px-2 py-1 rounded-full cursor-pointer"><i class="${heartIconClass} fa-heart text-red-500"></i></span>`;
 
     const comparePropertyData = encodeURIComponent(JSON.stringify(property));
     const isCompareActive = getCompareStatus(property.id);
     const compareIconClass = isCompareActive ? 'text-[color:var(--primary)]' : '';
 
     return `
-        <div onclick="window.location.href='/property/${property.id}'" data-property-id="${property.id}" class="cursor-pointer border border-[color:var(--border-color)] rounded-2md overflow-hidden rounded-2xl group relative transition-all duration-300">
+        <div onclick="window.location.href='/property/${property.id}'" 
+            data-property-id="${property.id}" 
+            class="cursor-pointer border border-[color:var(--border-color)] rounded-2xl overflow-hidden flex flex-col h-full group transition-all duration-300 relative">
+
+            <!-- Image -->
             <div class="relative overflow-hidden">
                 <img src="${property.media.path}" alt="${property.title}" class="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-105" />
-                <div class="absolute inset-0 flex items-center justify-center overflow-hidden">
-                    <div class="absolute inset-0 bg-black/40 transition-all duration-500 ease-in-out transform -translate-y-full opacity-0 group-hover:translate-y-0 group-hover:opacity-100 z-0"></div>
-                    <div class="z-10 flex items-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
-                </div>
-                <div class="absolute top-3 left-4 flex gap-1">
-                    ${addTypeBadge}
-                </div>
+                ${badges}
                 ${premiumBadge}
-
                 ${favoriteOrRemoveButton}
-                
             </div>
 
-            <div class="p-5">
-                <h3 class="font-bold text-lg sm:text-md text-[color:var(--text-color)] transition hover:text-[color:var(--primary)] mb-2">
-                    ${property.title}
-                </h3>
-              
-                <p class="text-sm sm:text-base md:text-[16px] text-[color:var(--grey-text)] flex items-center mb-2">
-                    <i class="fas fa-map-marker-alt mr-2"></i>
-                    ${property.address}
-                </p>
-                
-                <div class="flex items-center text-sm sm:text-base md:text-[16px] text-[#959699] gap-4 mb-4">
-                    <span><span class="text-[#2C2E33]">${property.beds}</span> Yataq</span>
-                    <span><span class="text-[#2C2E33]">${property.baths}</span> Hamam</span>
-                    <span><span class="text-[#2C2E33]">${property.area}</span> Kvm</span>
+            <!-- Card content -->
+            <div class="p-4 flex flex-col flex-1">
+                <div class="flex flex-col gap-2" style="min-height:120px;">
+                    <h3 class="font-bold text-[color:var(--text-color)] h-[30px] transition hover:text-[color:var(--primary)]">
+                        ${property.title}
+                    </h3>
+
+                    <p class="text-sm sm:text-base md:text-[16px] text-[color:var(--grey-text)] flex items-center mt-4 h-[40px]">
+                        <i class="fas fa-map-marker-alt mr-2"></i> ${property.address}
+                    </p>
+                    
+                    <div class="flex items-center text-sm sm:text-base md:text-[16px] text-[#959699] gap-4">
+                        <span><span class="text-[#2C2E33]">${property.beds}</span> Yataq</span>
+                        <span><span class="text-[#2C2E33]">${property.baths}</span> Hamam</span>
+                        <span><span class="text-[#2C2E33]">${property.area}</span> Kvm</span>
+                    </div>
+
+                    <span class="text-sm text-gray-500">${property.buildingType}</span>
                 </div>
-                <span class="text-sm text-gray-500 ">${property.buildingType}</span>
-                            
-                <div class="flex justify-between py-2 mt-2 items-center border-t border-[color:var(--border-color)] pt-4">
+
+                <!-- Price və Compare button -->
+                <div class="flex justify-between py-2 mt-auto items-center border-t border-[color:var(--border-color)] pt-4">
                     <span class="text-[color:var(--primary)] font-bold text-base sm:text-lg">${price} AZN</span>
                     <button onclick="event.stopPropagation(); toggleCompare(this, decodeURIComponent('${comparePropertyData}'));" class="flex compare items-center gap-1 text-sm text-[#2C2E33] hover:text-[color:var(--primary)] transition-colors">
                         <i class="fas fa-random ${compareIconClass}"></i> Müqayisə
                     </button>
                 </div>
             </div>
-        </div>`;
+        </div>
+    `;
 }
 
 function getFavoriteStatus(propertyId) {
@@ -106,7 +116,7 @@ window.toggleFavorite = async function (element, propertyJsonString) {
     if (token) {
         const url = '/api/favorite';
         const method = isAlreadyFavorite ? 'DELETE' : 'POST';
-        const body = JSON.stringify({property_id: property.id});
+        const body = JSON.stringify({ property_id: property.id });
 
         try {
             const response = await fetch(url, {
