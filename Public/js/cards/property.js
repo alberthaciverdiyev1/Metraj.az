c2c001187f2212f6b32ec7309b8618a854e76a78
leyla339 <leyla_bayramova_05@bk.ru>
2025-08-27 23:16:08 +0400

    update

--- Public/js/cards/property.js
+++ Public/js/cards/property.js
@@ -20,7 +20,7 @@ export function propertyCard(property, showRemoveButton = false) {
   // console.log(property);
 
   const badges = `
-<div class="absolute top-3 left-3 flex flex-wrap  gap-2">
+<div class="flex flex-wrap  gap-2">
     ${
       property.add_type === "rent"
         ? `<span class="bg-[color:var(--primary)] text-white text-xs font-semibold px-2 text-center py-1 rounded-full">Kirayə</span>`
@@ -56,7 +56,6 @@ export function propertyCard(property, showRemoveButton = false) {
   const premiumBadge = property.is_premium
     ? `<span class="absolute top-3 right-4 text-black font-semibold text-md bg-white px-2 py-1 rounded-full"><i class="fa-solid fa-crown"></i></span>`
     : "";
-
   const heartIconClass = getFavoriteStatus(property.id)
     ? "fa-solid"
     : "fa-regular";
@@ -78,21 +77,20 @@ export function propertyCard(property, showRemoveButton = false) {
             <div class="relative overflow-hidden">
                 <img src="${property.media.path}" alt="${property.title}" class="w-full h-64 object-cover transition-transform duration-500 group-hover:scale-105" />
                 ${premiumBadge}
-                ${badges}
                 ${favoriteOrRemoveButton}
             </div>
 
             <!-- Card content -->
             <div class="p-4 flex flex-col flex-1">
                 <div class="flex flex-col gap-2" style="min-height:120px;">
-                    <h3 class="font-bold text-[color:var(--text-color)] h-[30px] transition hover:text-[color:var(--primary)]">
+                    <h3 class="font-bold text-[color:var(--text-color)] h-[50px] transition hover:text-[color:var(--primary)]">
                         ${property.title}
                     </h3>
-
+  ${badges}
                     <p class="text-sm sm:text-base md:text-[16px] text-[color:var(--grey-text)] flex items-center mt-4 h-[40px]">
                         <i class="fas fa-map-marker-alt mr-2"></i> ${property.address}
                     </p>
-                    
+             
                     <div class="flex items-center text-sm sm:text-base md:text-[16px] text-[#959699] gap-4">
                         <span><span class="text-[#2C2E33]">${property.beds}</span> Yataq</span>
                         <span><span class="text-[#2C2E33]">${property.baths}</span> Hamam</span>
@@ -106,8 +104,8 @@ export function propertyCard(property, showRemoveButton = false) {
                 <div class="flex justify-between py-2 mt-auto items-center border-t border-[color:var(--border-color)] pt-4">
                     <span class="text-[color:var(--primary)] font-bold text-base sm:text-lg">${price} AZN</span>
                     <button onclick="event.stopPropagation(); toggleCompare(this, decodeURIComponent('${comparePropertyData}'));" class="flex compare items-center gap-1 text-sm text-[#2C2E33] hover:text-[color:var(--primary)] transition-colors">
-                        <i class="fas fa-random ${compareIconClass}"></i> Müqayisə
-                    </button>
+                        <i class="fa-solid fa-scale-balanced" ${compareIconClass}"></i> Müqayisə
+                    </button>              
                 </div>
             </div>
         </div>
