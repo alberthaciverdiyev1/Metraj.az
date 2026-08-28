28a3bf88ee95694fff3e16d3d8bf2d1f0a26978b
albert <alberthaciverdiyev55@gmail.com>
2026-08-28 11:13:19 +0400

    style(admin): change Filament admin primary color to Teal

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,7 +37,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogo(asset('images/kibriskarelogo1.png'))
             ->brandLogoHeight('2.2rem')
             ->colors([
-                'primary' => Color::Orange,
+                'primary' => Color::Teal,
             ])
             ->userMenuItems([
                 'profile' => MenuItem::make()
