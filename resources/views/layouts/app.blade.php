c11c00108556974e8b488939c08154878363284c
albert <alberthaciverdiyev55@gmail.com>
2026-08-28 11:37:53 +0400

    feat(favicon): add site favicons and rebuild frontend assets
    - Add favicon/site.webmanifest links in app.blade.php with new favicon files
    - Rebuild Tailwind CSS (app-DeqExfJw.css) and update Vite manifest
    - Rewrite global.js and add-form.js helpers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,14 @@
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('vendor/toastify/toastify.min.css') }}">
 
+
+    <link rel="icon" type="image/png" href="{{asset("images/favicons/favicon-96x96.png")}}" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="{{asset("images/favicons/favicon.svg")}}" />
+    <link rel="shortcut icon" href="{{asset("images/favicons/favicon.ico")}}" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset("images/favicons/apple-touch-icon.png")}}" />
+    <meta name="apple-mobile-web-app-title" content="KibrisKare" />
+    <link rel="manifest" href="{{asset("images/favicons/site.webmanifest")}}" />
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="stylesheet" href="{{ asset('css/app-legacy.css') }}?v=1.3">
